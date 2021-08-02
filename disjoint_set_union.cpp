dd12e5dab26b8e9d6f93605d0867434776aaf670
Siddhartha Hazra <siddhartharasa@gmail.com>
2021-08-02 09:28:08 +0530

    Update disjoint_set_union.cpp

--- disjoint_set_union.cpp
+++ disjoint_set_union.cpp
@@ -1,36 +1,17 @@
-class unionFind {
-public:
-  int *parent, *rank;
-  int set_size;
-  unionFind(int n = N) {
-    set_size = n;
-    parent = new int[n + 1];
-    rank = new int[n + 1];
-    for (int i = 0; i <= n; i++) {
-      parent[i] = i;
-      rank[i] = 1;
-    }
-  }
-  int root(int idx) {
-    int x = idx;
-    while (x != parent[x]) {
-      parent[x] = parent[parent[x]];
-      x = parent[x];
-    }
-    return x;
-  }
-  bool uni(int a, int b) {
-    int x = root(a), y = root(b);
-    if (rank[x] < rank[y]) {
-      swap(x, y);
-    }
-    if (x != y) {
-      parent[y] = x;
-      rank[x] += rank[y];
-      rank[y] = 0;
-      return false;
-      set_size -= 1;
-    }
-    return true;
-  }
+struct UnionFind {
+   int n, set_size, *parent, *rank;
+   UnionFind(){}
+   UnionFind(int a) {n = set_size = a;parent = new int[n+1];rank = new int[n+1];
+       for(int i = 1; i <= n; ++i) parent[i]=i, rank[i]=1;}
+   int find(int x) {if (x != parent[x]) return parent[x] = find(parent[x]);return x;}
+   void merge(int x, int y) {int xroot = find(x), yroot = find(y);
+       if (xroot != yroot) {
+           if (rank[xroot] >= rank[yroot]) {parent[yroot] = xroot;rank[xroot] += rank[yroot];}
+           else {parent[xroot] = yroot;rank[yroot] += rank[xroot];}
+           set_size -= 1;}}
+   void reset(){
+       set_size=n;for(int i=1;i<=n;i++)parent[i]=i,rank[i]=1;
+   }
+   int size() { return set_size; }
+   void print() {for(int i = 1; i <= n; ++i)cout << i << " -> " << parent[i] << "\n";}
 };

9a91bbdf8cc72c89f6359bd3352286bc23d2f054
Siddhartha Hazra <siddhartharasa@gmail.com>
2021-10-29 20:50:09 +0530

    Update disjoint_set_union.cpp

--- disjoint_set_union.cpp
+++ disjoint_set_union.cpp
@@ -1,15 +1,17 @@
-struct DSU {
-	vector<int> e;
-	void init (int n) { e = vector<int> (n, -1); }
-	int get (int x) { return (e[x] < 0 ? x : e[x] = get(e[x])); }
-	bool sameSet (int x, int y) { return get(x) == get(y); }
-	int size (int x) { return -e[get(x)]; }
-	bool unite (int x, int y) {
-		x = get(x), y = get(y);
-		if (x == y) return 0;
-		if (e[x] > e[y]) swap(x, y);
-		e[x] += e[y];
-		e[y] = x;
-		return 1;
-	}
+struct UnionFind {
+   int n, set_size, *parent, *rank;
+   UnionFind(){}
+   UnionFind(int a) {n = set_size = a;parent = new int[n+1];rank = new int[n+1];
+       for(int i = 1; i <= n; ++i) parent[i]=i, rank[i]=1;}
+   int find(int x) {if (x != parent[x]) return parent[x] = find(parent[x]);return x;}
+   void merge(int x, int y) {int xroot = find(x), yroot = find(y);
+       if (xroot != yroot) {
+           if (rank[xroot] >= rank[yroot]) {parent[yroot] = xroot;rank[xroot] += rank[yroot];}
+           else {parent[xroot] = yroot;rank[yroot] += rank[xroot];}
+           set_size -= 1;}}
+   void reset(){
+       set_size=n;for(int i=1;i<=n;i++)parent[i]=i,rank[i]=1;
+   }
+   int size() { return set_size; }
+   void print() {for(int i = 1; i <= n; ++i)cout << i << " -> " << parent[i] << "\n";}
 };

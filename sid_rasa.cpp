6be1836996e52f0332ad6eee7db0d1cf9248e08e
Siddhartha Hazra <siddhartharasa@gmail.com>
2022-01-09 11:48:04 +0530

    Add files via upload

---
+++ sid_rasa.cpp
@@ -0,0 +1,134 @@
+/*
+ ______________________     _______ _______ _______ _______
+(  ____ \__   __(  __  \   (  ____ |  ___  |  ____ (  ___  )
+| (    \/  ) (  | (  \  )  | (    )| (   ) | (    \/ (   ) |
+| (_____   | |  | |   ) |  | (____)| (___) | (_____| (___) |
+(_____  )  | |  | |   | |  |     __)  ___  (_____  )  ___  |
+      ) |  | |  | |   ) |  | (\ (  | (   ) |     ) | (   ) |
+/\____) |__) (__| (__/  )  | ) \ \_| )   ( /\____) | )   ( |
+\_______)_______(______/___|/   \__//     \_______)/     \|
+                      (_____)
+One day I will be Candidate Master on Codeforces
+I didn't come this far to only come this far */
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define pb push_back
+#define mp make_pair
+#define fl(i, a, b) for (int i = a; i < b; i++)
+#define vi vector<int>
+#define e1(a) int a; cin>>a;
+#define e2(a,b) int a,b; cin>>a>>b;
+#define e3(a,b,c) int a,b,c; cin>>a>>b>>c;
+#define av(arr,n) vector<int> arr(n); fl(i,0,n) cin>>arr[i];
+#define aa(arr,n) int arr[n]; fl(i,0,n) cin>>arr[i];
+#define es(s) string(s); cin >> (s);
+#define rz resize
+#define vvi vector<vector<int>>
+#define sz(s) s.size()
+#define mod 1000000007
+#define ff first
+#define ss second
+#define inf 10e15
+#define all(x) (x).begin(), (x).end()
+#define pii pair<int, int>
+#define mii map<int,int>
+#define st(a, n) memset(a, n, sizeof(a))
+#define vl(n) cout << n << "\n"
+#define vs(n) cout << n << " "
+#define el cout<<"\n"
+#define rmin(a,b) (a=min((a),(b)))
+#define rmax(a,b) (a=max((a),(b)))
+#define UB upper_bound
+#define LB lower_bound
+#define vn(n) cout << n
+#define dsc greater<int>()
+#define ps(x,y)  fixed<<setprecision(y)<<x
+#define R return
+#define B break
+#define C continue
+#define YC cout<<"YES"<<"\n"
+#define YS cout<<"Yes"<<"\n"
+#define NC cout<<"NO"<<"\n"
+#define fe(a) for(auto e:(a))
+#define NS cout<<"No"<<"\n"
+#define lcm(a,b) (a/__gcd(a,b))*b
+#define pa(a) for(auto e:a)cout<<e<<" "
+const int N = 1e5 + 5;
+int dx[4] = { -1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+int kx[8] = { -1, 1, 0, 0, -1, -1, 1, 1};
+int ky[8] = {0, 0, -1, 1, -1, 1, -1, 1};
+
+#ifndef ONLINE_JUDGE
+#define pra(a,n)cerr<<#a<<":";for(int i=0;i<n;i++)cerr<<a[i]<<" ";cerr<<endl;
+#define prm(mat,row,col)cerr<<#mat<<":\n";for(int i=0;i<row;i++){for(int j=0;j<col;j++)cerr<<mat[i][j]<<" ";cerr<<endl;}
+#define pr(...)dbs(#__VA_ARGS__,__VA_ARGS__)
+template<class S, class T>ostream &operator<<(ostream &os, const pair<S, T> &p) {return os << "(" << p.first << "," << p.second << ")";}
+template<class T>ostream &operator<<(ostream &os, const vector<T> &p) {os << "["; for (auto&it : p)os << it << " "; return os << "]";}
+template<class T>ostream &operator<<(ostream &os, const set<T>&p) {os << "["; for (auto&it : p)os << it << " "; return os << "]";}
+template<class T>ostream &operator<<(ostream &os, const multiset<T>&p) {os << "["; for (auto&it : p)os << it << " "; return os << "]";}
+template<class S, class T>ostream &operator<<(ostream &os, const map<S, T>&p) {os << "["; for (auto&it : p)os << it << " "; return os << "]";}
+template<class T>void dbs(string str, T t) {cerr << str << ":" << t << "\n";}
+template<class T, class...S>void dbs(string str, T t, S... s) {int idx = str.find(','); cerr << str.substr(0, idx) << ":" << t << ","; dbs(str.substr(idx + 1), s...);}
+#else
+#define pr(...){}
+#define pra(a,n){}
+#define prm(mat,row,col){}
+#endif
+
+int setBit(int n) {
+	int c = 0;
+	while (n > 0) {
+		if (n & 1 > 0)
+			c++;
+		n /= 2;
+	}
+	return c;
+}
+int minVal(int a, int b) {
+	int x = a;
+	int rem = setBit(x) - setBit(b);
+	int val = 0;
+	if (rem > 0) {
+		int pos = 0;
+		while (rem) {
+			if ((x >> pos) & 1 > 0) {
+				val += (1 << pos);
+				rem--;
+			}
+			pos++;
+		}
+		x -= val;
+	}
+	else {
+		rem = abs(rem);
+		int pos = 0;
+		while (rem) {
+			if ((x >> pos) & 1 == 0) {
+				val += (1 << pos);
+				rem--;
+			}
+			pos++;
+		}
+		x += val;
+	}
+	return x;
+}
+
+void solve()
+{
+	e2(a, b);
+	vl(minVal(a, b));
+}
+
+int32_t main()
+{
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solve();
+
+	return 0;
+}
